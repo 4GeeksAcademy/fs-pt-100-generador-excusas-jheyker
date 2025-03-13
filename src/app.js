2be7c5fcd3e29f734e13b632d4ad108c321e09a4
Jheyker13 <jheyker13@gmail.com>
2025-03-13 11:40:23 +0000

    generador-de-excusas

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const numeroAleatorio = (arr) => {
+  return Math.floor(Math.random()* arr.length)
+}
+console.log(numeroAleatorio(who));
+ 
+const asignar =(arr,index)=>{
+   return arr[index]
+}
+console.log(asignar(who,numeroAleatorio(who)));
+
+const encadenar = (fraseUno,fraseDos,fraseTres,fraseCuatro) => {
+  return `${fraseUno} ${fraseDos} ${fraseTres} ${fraseCuatro}`
+
+}
+  
+console.log(encadenar(asignar(who,numeroAleatorio(who)),asignar(action,numeroAleatorio(action)),asignar(what,numeroAleatorio(what)),asignar(when,numeroAleatorio(when))));
+
+document.getElementById("excuse").innerHTML= encadenar(asignar(who,numeroAleatorio(who)),asignar(action,numeroAleatorio(action)),asignar(what,numeroAleatorio(what)),asignar(when,numeroAleatorio(when)));
